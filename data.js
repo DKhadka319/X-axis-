e96e463bd1c91fd547b742a63f41677e6255b9e5
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2021-12-29 14:46:33 +1100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+export const Work = [
+    {
+        id:1,
+        name:"Intense Cravings",
+        description:"It is a restaurant website with ordering facilities and payment. It is built on the top of the Wodpress, with styled-components, searching and filtering functionalities, and fire animations.",
+        tags:["Wordpress","sass"],
+        
+        demo:"https://www.intensecravings.com/",
+        
+    },
+    {
+        id:2,
+        name:"Portfolio Website",
+        description:"This is the same website you using right now which is built using Reactjs and styled components. It has really smooth animations.",
+        tags:["react","react-router","styledComponent"],
+        
+        demo:"http://www.dhirajkhadka.com",
+        github:"https://github.com/odebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"DK Star ",
+        description:"This is an e-commerce website where we can get custom items. It is built on the top of the Wodpress, with CSS, and searching and filtering functionalities. ",
+        tags:["Wordpress","sass","php"],
+        
+        demo:"https://dk-star.com/",
+        github:"https://github.com/codebucks27/devto-clone"
+    },
+    {
+        id:4,
+        name:"DK Lists",
+        description:"This is a blog website where we can get a blog about shoes. It is built on the top of the Wodpress, with CSS, and responsive animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"http://dklists.com/",
+        github:"https://github.com/codebucks27/react-sidebar"        
+    },
+    
+  
+] 
